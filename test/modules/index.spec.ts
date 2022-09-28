8e6393860fafd3a41fa1d40b38e73c7bd1b663fd
Bennett Benedict <bennybenester@gmail.com>
2022-09-28 23:58:36 +0300

    test: add tests for login feature

---
+++ test/modules/index.spec.ts
@@ -0,0 +1,16 @@
+import sarufi from '../../src/modules/index';
+import {
+  ErrorResponse,
+  LoginResponse,
+} from '../../src/shared/interfaces/auth.interface';
+jest.setTimeout(300000);
+describe('Login', () => {
+  it('Should return message and token', async () => {
+    const result: LoginResponse | ErrorResponse = await sarufi.login(
+      '<username>',
+      '<password>'
+    );
+    expect(result.message).toBeDefined();
+    expect(result.token).toBeDefined();
+  });
+});
