62e40137dd52589b7b7b9ce84e21606eaea97684
Bennett Benedict <bennybenester@gmail.com>
2022-09-29 01:48:05 +0300

    feat: add public api to export all instances

---
+++ test/shared/error.helper.spec.ts
@@ -0,0 +1,15 @@
+import { sanitizeLoginError } from '../../src/shared/helpers/error.helper';
+import { AxiosError } from 'axios';
+describe('Login', () => {
+  it('Should return a sanitized Error', () => {
+    const error = {
+      message: 'An error occured',
+      response: { data: {} },
+      code: 'FAILED',
+      status: 404,
+    };
+    const errorResponse = sanitizeLoginError(error as unknown as AxiosError);
+    expect(errorResponse.message).toBeDefined();
+    expect(errorResponse.success).toBe(false);
+  });
+});
