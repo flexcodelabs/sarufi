38e6bc29ed0d12238928841b181a9ed6fb2fe9ee
Bennett Benedict <bennybenester@gmail.com>
2022-09-28 23:57:18 +0300

    feat: add helper for sanitizing error response

---
+++ src/shared/helpers/error.helper.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosError } from 'axios';
+import { ErrorResponse } from '../interfaces/auth.interface';
+
+const sanitizedAxiosError = (
+  error: AxiosError,
+  serverError: AxiosError<ErrorResponse>
+) => {
+  if (serverError?.response) {
+    return {
+      ...serverError?.response?.data,
+      code: serverError?.code || 'FAILED',
+      status: serverError?.response?.status || 404,
+      success: false,
+    };
+  }
+  return {
+    ...error,
+    success: false,
+    message: error?.message,
+    token: '',
+    status: 404,
+  };
+};
+export const sanitizeLoginError = (error: AxiosError): ErrorResponse => {
+  const serverError = error as AxiosError<ErrorResponse>;
+  if (axios.isAxiosError(error)) {
+    return sanitizedAxiosError(error, serverError);
+  }
+  return {
+    success: false,
+    message: serverError?.message || 'Internal server error',
+    token: '',
+    status: 500,
+    code: 'FAILED',
+  };
+};
