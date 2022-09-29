d7219cc426dba04cb58467e13bfabcd294cfb7a1
Bennett Benedict <bennybenester@gmail.com>
2022-09-29 15:34:00 +0300

    Add create bot and get bots functionalities

--- src/modules/instances.ts
+++ src/modules/instances.ts
@@ -1,13 +1,31 @@
+import { LoginResponse } from '../shared/interfaces/auth.interface';
 import {
-  ErrorResponse,
-  LoginResponse,
-} from '../shared/interfaces/auth.interface';
+  BotRequest,
+  BotResponse,
+  BotsResponse,
+} from '../shared/interfaces/bot.interface';
+import { ErrorResponse } from '../shared/interfaces/shared.interface';
 import { Sarufi } from './sarufi';
 
 export const login = async (
   username: string,
-  password: string
+  password: string,
+  url?: string
 ): Promise<LoginResponse | ErrorResponse> => {
-  const sarufi = new Sarufi();
+  const sarufi = new Sarufi(url);
   return await sarufi.login(username, password);
+};
+
+export const createBot = async (
+  bot: BotRequest,
+  url?: string
+): Promise<BotResponse | ErrorResponse> => {
+  const sarufi = new Sarufi(url);
+  return await sarufi.createBot(bot);
+};
+export const geBots = async (
+  url?: string
+): Promise<ErrorResponse | BotsResponse> => {
+  const sarufi = new Sarufi(url);
+  return await sarufi.geBots();
 };
--- src/modules/sarufi.ts
+++ src/modules/sarufi.ts
@@ -1,11 +1,20 @@
 import axios, { AxiosError } from 'axios';
-import { sanitizeLoginError } from '../shared/helpers/error.helper';
+import { sanitizeErrorResponse } from '../shared/helpers/error.helper';
+import { LoginResponse } from '../shared/interfaces/auth.interface';
 import {
-  ErrorResponse,
-  LoginResponse,
-} from '../shared/interfaces/auth.interface';
+  BotRequest,
+  BotResponse,
+  BotsResponse,
+} from '../shared/interfaces/bot.interface';
+import { ErrorResponse } from '../shared/interfaces/shared.interface';
+
+declare let global: { token: string | undefined; url: string | undefined };
+
 export class Sarufi {
-  private BASE_DOMAIN = 'https://api.sarufi.io';
+  constructor(private url?: string) {
+    global.url = this.url;
+  }
+  private BASE_DOMAIN = global.url || 'https://api.sarufi.io';
   login = async (
     username: string,
     password: string
@@ -17,9 +26,54 @@ export class Sarufi {
           password,
         })
       ).data;
+      if (global) {
+        global.token = data.token;
+      }
       return { ...data, success: true };
     } catch (error) {
-      return sanitizeLoginError(error as AxiosError);
+      return sanitizeErrorResponse(error as AxiosError);
+    }
+  };
+  createBot = async (bot: BotRequest): Promise<BotResponse | ErrorResponse> => {
+    if (global?.token) {
+      return await this.createUserBot(bot, global?.token);
+    }
+    return { success: false, bot: undefined, message: 'Unauthorized' };
+  };
+  geBots = async (): Promise<ErrorResponse | BotsResponse> => {
+    if (global?.token) {
+      return await this.getUserBots(global?.token);
+    }
+    return { success: false, bots: [], message: 'Unauthorized' };
+  };
+
+  getUserBots = async (
+    token: string
+  ): Promise<ErrorResponse | BotsResponse> => {
+    try {
+      const bots = (
+        await axios.get(`${this.BASE_DOMAIN}/chatbots`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+      ).data;
+      return { success: true, bots };
+    } catch (e) {
+      return sanitizeErrorResponse(e as AxiosError);
+    }
+  };
+  createUserBot = async (
+    bot: BotRequest,
+    token: string
+  ): Promise<BotResponse | ErrorResponse> => {
+    try {
+      const createdBot = (
+        await axios.post(`${this.BASE_DOMAIN}/chatbot`, bot, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+      ).data;
+      return { success: true, bot: createdBot };
+    } catch (e) {
+      return sanitizeErrorResponse(e as AxiosError);
     }
   };
 }
