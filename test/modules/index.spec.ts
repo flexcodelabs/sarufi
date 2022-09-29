8cc55fbc56dd50a760bdbd3a2415edb568e6f3c9
Bennett Benedict <bennybenester@gmail.com>
2022-09-29 15:36:49 +0300

    rename error sanitizer function

--- test/modules/index.spec.ts
+++ test/modules/index.spec.ts
@@ -1,16 +1,103 @@
 import { sarufi } from '../../src';
-import {
-  ErrorResponse,
-  LoginResponse,
-} from '../../src/shared/interfaces/auth.interface';
+import { LoginResponse } from '../../src/shared/interfaces/auth.interface';
+import axios from 'axios';
+import xhrAdapter from 'axios/lib/adapters/xhr';
+import HttpRequestMock from 'http-request-mock';
+import { ErrorResponse } from '../../src/shared/interfaces/shared.interface';
+import { Bot, BotRequest } from '../../src/shared/interfaces/bot.interface';
+import { BotData, BotsData } from '../mocks/mocks.constants';
+
 jest.setTimeout(300000);
-describe('Login', () => {
+
+axios.defaults.adapter = xhrAdapter;
+const mocker = HttpRequestMock.setupForUnitTest('xhr');
+const BASE_DOMAIN = 'https://api.sarufi.io';
+
+//Mock succesfull
+describe('Successful Login', () => {
+  const loginDTO = {
+    success: true,
+    message: 'Logged in successfully',
+    token: 'basic token',
+  };
+
+  mocker.post(`${BASE_DOMAIN}/users/login`, ():
+    | LoginResponse
+    | ErrorResponse => {
+    return loginDTO;
+  });
   it('Should return message and token', async () => {
-    const result: LoginResponse | ErrorResponse = await sarufi.login(
-      '<username>',
-      '<password>'
-    );
+    const result: LoginResponse | ErrorResponse = await sarufi.login('', '');
     expect(result.message).toBeDefined();
-    expect(result.token).toBeDefined();
+    expect(result).toMatchObject(loginDTO);
   });
 });
+
+describe('Successful Bot creation', () => {
+  mocker.post(`${BASE_DOMAIN}/chatbot`, (): BotRequest => {
+    return BotData;
+  });
+  it('Should return a created bot given a valid token', async () => {
+    const createdBot = await sarufi.createBot({
+      name: '',
+    });
+    expect(createdBot.success).toBe(true);
+    expect(createdBot.bot).toMatchObject(BotData);
+  });
+});
+
+describe('Successful Get Bots', () => {
+  mocker.get(`${BASE_DOMAIN}/chatbots`, (): Bot[] => {
+    return BotsData;
+  });
+  it('Should return user bots', async () => {
+    const userBots = await sarufi.geBots();
+    expect(userBots.success).toBe(true);
+    expect(userBots.bots?.length).toBe(2);
+  });
+});
+
+/*
+describe('Successful Login', () => {
+  const loginDTO = {
+    success: false,
+    message: 'Logged in failed',
+    token: null,
+  };
+  mocker.post(`${BASE_DOMAIN}/users/login`, (): {
+    token: null;
+    success: boolean;
+  } => {
+    return { token: null, success: false };
+  });
+  it('Should return message and token', async () => {
+    const result: LoginResponse | ErrorResponse = await sarufi.login('', '');
+    console.log(result);
+    expect(result.message).toBeDefined();
+  });
+});
+
+describe('Failed Bot creation', () => {
+  mocker.post(`${BASE_DOMAIN}/chatbot`, (): BotRequest => {
+    return BotData;
+  });
+  it('Should return a created bot given a valid token', async () => {
+    const createdBot = await sarufi.createBot({
+      name: '',
+    });
+    console.log(createdBot);
+    expect(createdBot.success).toBe(false);
+    expect(createdBot.bot).toBe(undefined);
+  });
+});
+
+describe('Failed Get Bots', () => {
+  mocker.get(`${BASE_DOMAIN}/chatbots`, (): Bot[] => {
+    return BotsData;
+  });
+  it('Should return user bots', async () => {
+    const userBots = await sarufi.geBots();
+    expect(userBots.success).toBe(false);
+    expect(userBots.bots?.length).toBe(0);
+  });
+});*/
--- test/shared/error.helper.spec.ts
+++ test/shared/error.helper.spec.ts
@@ -1,4 +1,4 @@
-import { sanitizeLoginError } from '../../src/shared/helpers/error.helper';
+import { sanitizeErrorResponse } from '../../src/shared/helpers/error.helper';
 import { AxiosError } from 'axios';
 describe('Login', () => {
   it('Should return a sanitized Error', () => {
@@ -8,7 +8,7 @@ describe('Login', () => {
       code: 'FAILED',
       status: 404,
     };
-    const errorResponse = sanitizeLoginError(error as unknown as AxiosError);
+    const errorResponse = sanitizeErrorResponse(error as unknown as AxiosError);
     expect(errorResponse.message).toBeDefined();
     expect(errorResponse.success).toBe(false);
   });
